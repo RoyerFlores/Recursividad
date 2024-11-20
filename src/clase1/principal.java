b3957469dda95996f2f39ffb2e356c248538748e
Royer <Royer@172.24.5.133>
2024-11-20 16:50:36 -0400

    recursividad

--- src/clase1/principal.java
+++ src/clase1/principal.java
@@ -13,13 +13,20 @@ public class principal {
     public static void main(String[] args) {
         //VERIFICACION RECURSION EN FUNCIONES
         //FACTORIAL
+        System.out.println("Factorial de 5");
         System.out.println(factorial(5));
         //SUMA DE PARES
+        System.out.println("Suma de 3 pares");
         System.out.println(sumP(3));
         //SUMA DE N IMPARES
-        //SUMA DE LOS N MULTIPLOS DE X
-        //SUMA DE LOS DIGITOS DE UN NUMERO
+        System.out.println("Suma de 3 impares");
         System.out.println(sumI(3));
+        //SUMA DE LOS N MULTIPLOS DE X
+        System.out.println("Suma de Múltiplos de 3");
+        System.out.println(multiplosX(3, 3));
+        //SUMA DE LOS DIGITOS DE UN NUMERO
+        System.out.println("Suma de dígitos de 1234");
+        System.out.println(digitos(1234));
     }
 
     public static int factorial(int x) {
@@ -43,6 +50,22 @@ public class principal {
             return 1;
         } else {
             return sumI(n - 1) + n * 2 - 1;
+        }
+    }
+
+    public static int multiplosX(int n, int m) {
+        if (m > 0) {
+            return n * m + multiplosX(n, m - 1);
+        } else {
+            return 0;
+        }
+    }
+
+    public static int digitos(int n) {
+        if (n > 10) {
+            return n % 10 + digitos(n / 10);
+        } else {
+            return n;
         }
     }
 }
